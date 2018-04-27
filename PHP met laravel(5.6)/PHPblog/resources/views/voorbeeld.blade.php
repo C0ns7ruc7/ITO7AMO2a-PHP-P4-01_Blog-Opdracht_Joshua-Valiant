e07ed3783b6137cd157b684241f0f9a6242992c4
C0ns7ruc7 <00093215@student.rocleiden.nl>
2018-04-27 12:31:20 +0200

    laravel lijst pagina aagngemaakt
    welkom pagina opgesplitst
    lijst pagina+route aangemaakt

---
+++ PHP met laravel(5.6)/PHPblog/resources/views/voorbeeld.blade.php
@@ -0,0 +1,6 @@
+@extends ('Partials.layout') {{-- uitbrijding, gebruikt deze als basis structuur --}}
+
+@section('content')
+    {{-- deponeerd de content van wat hier tussen staat in de plek waar '@yield'
+         code staat in de include --}}
+@endsection
--- PHP met laravel(5.6)/PHPblog/routes/web.php
+++ PHP met laravel(5.6)/PHPblog/routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/lijst', function () {
+    return view('lijst');
+});
