9ed6ff8f070d77cf1b69328eb6564a54e167a53f
C0ns7ruc7 <00093215@student.rocleiden.nl>
2018-05-25 12:25:54 +0200

    1.0.2 2.0.2 3.0.2
    changed the index files to be pointless module parts.

---
+++ PHP met clasess/class/DatabaseConnectie.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vabese
+ * Date: 25-5-2018
+ * Time: 12:25
+ */
+
+class DatabaseConnectie
+{
+
+}
---
+++ PHP zonder classes en met PDO/partials/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Valiant Hekert">
+
+    <title>blog site</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #eee;
+            color: #555;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+            padding: 0.5%;
+        }
+    </style>
+</head>
+<body>
