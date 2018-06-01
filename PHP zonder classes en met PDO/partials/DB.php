79778d58d54c6129dd4bef8568b7ed85b5efcab6
C0ns7ruc7 <00093215@student.rocleiden.nl>
2018-06-01 13:39:40 +0200

    db update
    TODO: scap all DB.php files and redo them from scratch

--- PHP zonder classes en met PDO/partials/DB.php
+++ PHP zonder classes en met PDO/partials/DB.php
@@ -9,15 +9,22 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "php_database_simple";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
 }
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-?>
+$conn = null;
+echo "</table>";
--- PHP zonder classes en zonder PDO/partials/DB.php
+++ PHP zonder classes en zonder PDO/partials/DB.php
@@ -17,4 +17,18 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+$sql = "SELECT * FROM tasklist";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - title: " . $row["title"]. " - datum: " . $row["post_datum"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
