fcd0140334009ee85bbfb43da671ca6a11b6a2a6
Jmvdbsch <00125475@student.rocleiden.nl>
2018-06-01 13:26:24 +0200

    Formulier gemaakt in de index

--- PHP zonder classes en zonder PDO/index.php
+++ PHP zonder classes en zonder PDO/index.php
@@ -2,7 +2,17 @@
 include_once "partials/header.php";
 ?>
 
-    
+    <div class="body">
+        <div class="create-task">
+            <form class="create-task__form">
+                <label class="form-label titel-label">Titel:</label>
+                <input class="form-input titel-input" type="text" name="titel" placeholder="Titel">
+                <label class="form-label beschrijving-label">Omschrijving:</label>
+                <textarea class="form-input omschrijving-input" placeholder="Omschrijving"></textarea>
+                <input class="form-button" type="submit" value="Verzenden"/>
+            </form>
+        </div>
+    </div>
 
 <?php
 include_once "partials/header.php";
